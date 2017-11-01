eaef74123ccf80454e64a21723a2924164e9c1ec
Joshua Benton <tbsw_productions@hotmail.com>
2017-11-01 11:27:21 -0400

    employment history resized

--- assets/js/EmploymentGraph.js
+++ assets/js/EmploymentGraph.js
@@ -19,12 +19,13 @@ function drawChart() {
         [ 'Industry', 'Sagitec Solutions',  new Date(2017, 2, 1),  new Date() ],
         [ 'Industry', 'DAK Americas',  new Date(2016, 5, 1),  new Date(2016,8,30) ],
         [ 'Industry', 'Savanna River Remediation',  new Date(2015, 5, 1),  new Date(2015,8,30) ],
-        [ 'Industry', 'UCI',  new Date(2011, 5, 1),  new Date(2012,8,30) ],
+        //[ 'Industry', 'UCI',  new Date(2011, 5, 1),  new Date(2012,8,30) ],
         [ 'Academic', 'Teaching Assistant',  new Date(2015, 6, 1),  new Date(2017,1,1) ],
-        [ 'Robotics', 'Research Assistant (AFRL)',  new Date(2014, 10, 1),  new Date(2016,12,30) ],
-        [ 'Chemistry', 'Research Assistant (Neurotransmitter)',  new Date(2015, 7, 1),  new Date(2016,8,30) ],
+        //[ 'Robotics', 'Research Assistant (AFRL)',  new Date(2014, 10, 1),  new Date(2016,12,30) ],
+        //[ 'Chemistry', 'Research Assistant (Neurotransmitter)',  new Date(2015, 7, 1),  new Date(2016,8,30) ],
+        [ 'Research', 'Research Assistant',  new Date(2014, 10, 1),  new Date(2016,12,30) ],
         [ 'Contract Work', 'Private Tutor',  new Date(2016, 1, 1),  new Date(2017,6,1) ],
-        [ 'Industry 2', 'Leapforce',  new Date(2013, 5, 1),  new Date(2015,10,30) ],
+        [ 'Contract Work', 'Leapforce',  new Date(2013, 5, 1),  new Date(2015,10,30) ],
         [ 'Entrepreneurial', 'Style Connect',  new Date(2016, 6, 1),  new Date(2017,8,1) ],
         [ 'Entrepreneurial', 'BluechipAI',  new Date(2017, 8, 1),  new Date() ]
 
@@ -32,8 +33,19 @@ function drawChart() {
     ]);
 
     var options = {
-        timeline: { showRowLabels: true }
+        timeline: { showRowLabels: true },
+        'height':400
     };
+
 
     chart.draw(dataTable, options);
 }
+$(window).on('resizeEnd', function() {
+    drawChart(data);
+});
+$(window).resize(function() {
+    if(this.resizeTO) clearTimeout(this.resizeTO);
+    this.resizeTO = setTimeout(function() {
+        $(this).trigger('resizeEnd');
+    }, 500);
+});
